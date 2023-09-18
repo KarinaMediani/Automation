71c9b4772de7b75f970c0666fe78c4310f4c6a2f
Karina Martinatto Mediani <karina.mediani@MJV.net>
2023-09-18 07:52:13 -0300

    first commit

---
+++ IdeaProjects/TesteAPI/src/test/java/ServeRestTest.java
@@ -0,0 +1,99 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+
+class ServeRestTest {
+    @Test
+    @Order(1)
+    public void testDadoEmailSenhaValidosQuandoFacoLoginEntaoValidoStatusCodeSucesso(){
+        baseURI = "https://serverest.dev";
+
+        given()
+        .body("{\n" +
+                "    \"email\": \"fulano@qa.com\",\n" +
+                "    \"password\": \"teste\"\n" +
+                "}")
+        .contentType(ContentType.JSON)
+        .when()
+        .post("/login")
+        .then()
+        .statusCode(200)
+        .body("message", equalTo("Login realizado com sucesso"))
+        .extract()
+        .path("authorization");
+    }
+    @Test
+    @Order(2)
+    public void testDadoTokenValidoQuandoCadastroProdutoEntaoValidoStatusCodeSucesso(){
+        given()
+                .body("{\n" +
+                        "    \"nome\": \"Base de Computador\",\n" +
+                        "    \"preco\": 1200,\n" +
+                        "    \"descricao\": \"Base para refrigeração de notebook\",\n" +
+                        "    \"quantidade\": 50\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                //.header("Authorization", token)
+                .when()
+                .post("/produtos")
+                .then()
+                .statusCode(201)
+                .body("message", equalTo("Cadastro realizado com sucesso"))
+                .extract()
+                .path("_id");
+    }
+    @Test
+    @Order(3)
+    public void testDadoIdProdutoQuandoConsultoProdutoEntaoValidoStatusCodeSucessoSchema(){
+        given()
+                //.pathParam("id", idProduto)
+                .when()
+                .get("/produtos/{id}")
+                .then()
+                .statusCode(200)
+                .body("nome", equalTo("Base de Computador"))
+                .body("preco", equalTo(1200))
+                .body("descricao", equalTo("Base para refrigeração de notebook"))
+                .body("quantidade", equalTo(50));
+                //.body("_id", equalTo(idProduto));
+//                .assertThat()
+//                    .body(matchesJsonSchemaInClasspath("products-schema.json"));
+    }
+    @Test
+    @Order(4)
+    public void testDadoTokenValidoEIdProdutoQuandoEditoProdutoEntaoValidoStatusCodeSucesso(){
+        given()
+                //.pathParam("id", idProduto)
+                .body("{\n" +
+                        "    \"nome\": \"Base de Notebook\",\n" +
+                        "    \"preco\": 120,\n" +
+                        "    \"descricao\": \"Base para refrigeração de notebook\",\n" +
+                        "    \"quantidade\": 50\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+               // .header("Authorization", token)
+                .when()
+                .put("/produtos/{id}")
+                .then()
+                .statusCode(200)
+                .body("message", equalTo("Registro alterado com sucesso"));
+    }
+
+    @Test
+    @Order(5)
+    public void testDadoTokenValidoEIdProdutoQuandoDeletoProdutoEntaoValidoStatusCOdeSucesso(){
+        given()
+                //.pathParam("id", idProduto)
+                //.header("Authorization", token)
+                .when()
+                .delete("/produtos/{id}")
+                .then()
+                .statusCode(200)
+                .body("message", equalTo("Registro excluído com sucesso"));
+    }
+
+}
